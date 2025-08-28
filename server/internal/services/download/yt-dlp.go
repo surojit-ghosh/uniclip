e4e5cec26e542a7fae7677c348c1b87c95e65f6b
surojitghosh <contact@surojit.in>
2025-08-28 15:37:31 +0530

    hope it works

--- server/internal/services/download/yt-dlp.go
+++ server/internal/services/download/yt-dlp.go
@@ -2,16 +2,26 @@ package download
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func DownloadYouTubeVideo(url, output string) error {
+	isProduction := os.Getenv("ENV") == "production"
+
+	cookieFile := "../../../cookie.txt"
+	if isProduction {
+		cookieFile = "/cookies/yt-cookie.txt"
+	}
+
 	cmd := exec.Command("yt-dlp",
 		"--format", "best[ext=mp4]/best",
 		"--output", output,
 		"--no-playlist",
+		"--cookies", cookieFile,
 		url,
 	)
+
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("yt-dlp error:", string(out))
